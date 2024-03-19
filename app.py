07e76631beb772fe7dcb40fc2f8082c6c6a14e39
Rafael Estevam <rafestevam@gmail.com>
2024-03-19 19:31:21 -0300

    Config SQLAlchemy

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template, request, url_for, redirect
-
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/MYFLASK'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:C0c0m0l3!@localhost/FLASKAPP'
 
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 class Funcionario(db.Model):
     __tablename__='funcionario'
@@ -19,8 +18,10 @@ class Funcionario(db.Model):
         self.nome = nome
         self.email = email
 
-db.create_all()
+db.init_app(app)
 
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def index():
